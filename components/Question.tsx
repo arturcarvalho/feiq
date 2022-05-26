39dfef38f6f7f8dc03c54f50ad2f57c729a8182c
Artur Carvalho <arturbelico@gmail.com>
2022-05-26 18:30:47 +0100

    First

---
+++ components/Question.tsx
@@ -0,0 +1,7 @@
+type Props = {
+  children?: React.ReactNode;
+};
+
+export default function Question({ children }: Props) {
+  return <div>XX{children}XX</div>;
+}
--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    providerImportSource: "@mdx-js/react",
+  },
+});
+module.exports = withMDX({
+  // Append the default value with md extensions
+  pageExtensions: ["ts", "tsx", "mdx"],
+});
