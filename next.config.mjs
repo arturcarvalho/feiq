2237923329bc5172a0420259d91c3210ead35f0d
Artur Carvalho <arturbelico@gmail.com>
2022-06-09 07:37:16 +0200

    Add TOC

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,20 @@
 import rehypeHighlight from "rehype-highlight";
+import remarkToc from "remark-toc";
+import remarkSlug from "remark-slug";
 import mdx from "@next/mdx";
 
 const withMDX = mdx({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [rehypeHighlight],
-    // If you use `MDXProvider`, uncomment the following line.
-    providerImportSource: "@mdx-js/react",
-  },
+    extension: /\.mdx?$/,
+    options: {
+        remarkPlugins: [remarkSlug, remarkToc],
+        rehypePlugins: [rehypeHighlight],
+        // If you use `MDXProvider`, uncomment the following line.
+        providerImportSource: "@mdx-js/react",
+    },
 });
 
 export default withMDX({
-  reactStrictMode: true,
-  // Append the default value with md extensions
-  pageExtensions: ["ts", "tsx", "mdx"],
+    reactStrictMode: true,
+    // Append the default value with md extensions
+    pageExtensions: ["ts", "tsx", "mdx"],
 });
