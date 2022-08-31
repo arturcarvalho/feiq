4cab05844f4406d0cc94d1bac0dd923759581d90
Artur Carvalho <arturbelico@gmail.com>
2022-08-31 07:17:30 +0200

    Add questions' fragments

--- components/Question.tsx
+++ components/Question.tsx
@@ -1,11 +1,17 @@
 import styles from "../styles/Question.module.css";
+import React from "react";
 
 type Props = {
   children?: React.ReactNode;
+  slug: string;
 };
 
-export function Title({ children }: Props) {
-  return <h3>{children}</h3>;
+export function Title({ children, slug }: Props) {
+  return (
+    <h3 className={styles.title}>
+      {children} <a className={styles.slug} href={`#${slug}`}>#</a>
+    </h3>
+  );
 }
 
 export function Answer({ children }: Props) {
@@ -17,6 +23,17 @@ export function Answer({ children }: Props) {
   );
 }
 
-export function Question({ children }: Props) {
-  return <article className={styles.question}>{children}</article>;
+export function Question({ children, slug }: Props) {
+  const childrenWithProps = React.Children.map(children, (child) => {
+    // https://stackoverflow.com/questions/32370994/how-to-pass-props-to-this-props-children
+    if (React.isValidElement(child)) {
+      return React.cloneElement(child, { slug });
+    }
+    return child;
+  });
+  return (
+    <article className={styles.question} id={slug}>
+      {childrenWithProps}
+    </article>
+  );
 }
